fc14c4f2a9ed56e14fe24e52df4f70107ed71303
2101class <3223441671@qq.com>
2021-08-23 21:30:45 +0800

    项目的描述信息

---
+++ src/main/java/cn/baizhi/gittt/GitttApplication.java
@@ -0,0 +1,13 @@
+package cn.baizhi.gittt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi/gittt/GitttApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi.gittt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
